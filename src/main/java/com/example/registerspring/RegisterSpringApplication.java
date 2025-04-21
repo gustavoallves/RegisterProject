cd5aba4aa69dc3a33ad33dff5f6893d7843b5376
Gustavo <gu.alvesslv@gmail.com>
2025-04-21 17:22:49 -0300

    First commit

---
+++ src/main/java/com/example/registerspring/RegisterSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.registerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/registerspring/RegisterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
