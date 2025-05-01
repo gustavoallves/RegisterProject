ca897f8cce8491e6eab6149e5e61d26f2ba7c5e8
Gustavo <gu.alvesslv@gmail.com>
2025-05-01 18:43:17 -0300

    feat: Adiciona o ORM na MissionModel e no NinjaModel a partir do Repository

--- src/main/java/com/example/registerspring/ninjas/NinjaRepository.java
+++ src/main/java/com/example/registerspring/ninjas/NinjaRepository.java
@@ -1,4 +1,6 @@
 package com.example.registerspring.ninjas;
 
-public interface NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
