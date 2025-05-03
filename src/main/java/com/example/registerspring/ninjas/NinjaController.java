acea293a22baeb26d9d1d0084d06372c1cf89922
Gustavo <gu.alvesslv@gmail.com>
2025-05-03 20:11:46 -0300

    fix: Altera o nome da rota @RequestMapping do NinjaController

--- src/main/java/com/example/registerspring/ninjas/NinjaController.java
+++ src/main/java/com/example/registerspring/ninjas/NinjaController.java
@@ -3,7 +3,7 @@ package com.example.registerspring.ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("ninja")
 public class NinjaController {
 
     @GetMapping("/welcome")
