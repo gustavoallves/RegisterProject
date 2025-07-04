c799553adc908778f17c36a47138c9e89aae3856
Gustavo <gu.alvesslv@gmail.com>
2025-07-04 18:19:03 -0300

    fix: Muda excecao tratada em GlobalExceptionHandler Substituida ChangeSetPersister.NotFoundException por NotFoundException no metodo handleResourceNotFoundException para corrigir o tratamento de excecao e melhorar a manutencao do codigo.

--- src/main/java/com/example/registerspring/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/registerspring/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.registerspring.common.exception;
 
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
+    @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(NotFoundException exception) {
         ErrorResponse errorResponse = new ErrorResponse(
                 HttpStatus.NOT_FOUND.value(),
